2d54391c6a0fe65251515dfa335ac0fa8a162ad0
sauravyadav7721 <sauravyadav7721@gmail.com>
2026-07-05 00:13:28 +0530

    logger added

---
+++ main.py
@@ -0,0 +1,5 @@
+from drink_quality_prediction import logger
+
+
+logger.info("Starting the application...")
+
--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="src",
-    version="0.0.0",
-    author="Saurav Yadav",
-    author_email="sauravyadav7721@gmail.com",
-    packages=find_packages(),
-    install_requires=[]
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Drinks-Quality-Prediction-System"
+AUTHOR_USER_NAME = "Saurav Yadav"
+SRC_REPO = "drink_quality_prediction"
+AUTHOR_EMAIL = "sauravyadav7721@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
