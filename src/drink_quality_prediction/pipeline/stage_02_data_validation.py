66ee27c0314e1b788609a27ad41728d4a23d8282
sauravyadav7721 <sauravyadav7721@gmail.com>
2026-07-05 02:32:43 +0530

    data validation added

---
+++ src/drink_quality_prediction/pipeline/stage_02_data_validation.py
@@ -0,0 +1,17 @@
+from drink_quality_prediction.config.configuration import ConfigurationManager
+from drink_quality_prediction.components.data_validation import DataValiadtion
+from drink_quality_prediction import logger
+
+
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
+        
